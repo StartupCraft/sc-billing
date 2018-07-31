StartupCraft/sc-webhooks

initial install

Rakefile
@@ -1,0 +1,34 @@
+# frozen_string_literal: true
+
+begin
+  require 'bundler/setup'
+rescue LoadError
+  puts 'You must `gem install bundler` and `bundle install` to run rake tasks'
+end
+
+APP_RAKEFILE = File.expand_path('spec/dummy/Rakefile', __dir__)
+load 'rails/tasks/engine.rake'
+
+Bundler::GemHelper.install_tasks
+
+Dir[File.join(File.dirname(__FILE__), 'tasks/**/*.rake')].each { |f| load f }
+
+require 'rdoc/task'
+
+RDoc::Task.new(:rdoc) do |rdoc|
+  rdoc.rdoc_dir = 'rdoc'
+  rdoc.title    = 'Sc::Webhooks'
+  rdoc.options << '--line-numbers'
+  rdoc.rdoc_files.include('README.md')
+  rdoc.rdoc_files.include('lib/**/*.rb')
+end
+
+require 'bundler/gem_tasks'
+
+require 'rspec/core'
+require 'rspec/core/rake_task'
+
+desc 'Run all specs in spec directory (excluding plugin specs)'
+RSpec::Core::RakeTask.new(spec: 'app:db:test:prepare')
+
+task default: :spec

lib/sc/webhooks/engine.rb
@@ -1,0 +1,16 @@
+# frozen_string_literal: true
+
+module SC
+  module Webhooks
+    class Engine < ::Rails::Engine
+      initializer :append_migrations do |app|
+        app.config.paths['db/migrate'] += config.paths['db/migrate'].expanded unless app.root.to_s.match?(root.to_s)
+      end
+
+      config.generators do |g|
+        g.test_framework :rspec
+        g.fixture_replacement :factory_bot, dir: 'spec/factories'
+      end
+    end
+  end
+end

lib/sc/webhooks/railtie.rb
@@ -1,0 +1,8 @@
+# frozen_string_literal: true
+
+module SC
+  module Webhooks
+    class Railtie < ::Rails::Railtie
+    end
+  end
+end

lib/sc/webhooks/version.rb
@@ -1,0 +1,7 @@
+# frozen_string_literal: true
+
+module SC
+  module Webhooks
+    VERSION = '0.1.0'
+  end
+end

StartupCraft/sc-billing

Rakefile
@@ -1,0 +1,34 @@
+# frozen_string_literal: true
+
+begin
+  require 'bundler/setup'
+rescue LoadError
+  puts 'You must `gem install bundler` and `bundle install` to run rake tasks'
+end
+
+APP_RAKEFILE = File.expand_path('spec/dummy/Rakefile', __dir__)
+load 'rails/tasks/engine.rake'
+
+Bundler::GemHelper.install_tasks
+
+Dir[File.join(File.dirname(__FILE__), 'tasks/**/*.rake')].each { |f| load f }
+
+require 'rdoc/task'
+
+RDoc::Task.new(:rdoc) do |rdoc|
+  rdoc.rdoc_dir = 'rdoc'
+  rdoc.title    = 'Sc::Billing'
+  rdoc.options << '--line-numbers'
+  rdoc.rdoc_files.include('README.md')
+  rdoc.rdoc_files.include('lib/**/*.rb')
+end
+
+require 'bundler/gem_tasks'
+
+require 'rspec/core'
+require 'rspec/core/rake_task'
+
+desc 'Run all specs in spec directory (excluding plugin specs)'
+RSpec::Core::RakeTask.new(spec: 'app:db:test:prepare')
+
+task default: :spec

lib/sc/billing/engine.rb
@@ -1,0 +1,18 @@
+# frozen_string_literal: true
+
+module SC
+  module Billing
+    class Engine < ::Rails::Engine
+      isolate_namespace SC::Billing
+
+      initializer :append_migrations do |app|
+        app.config.paths['db/migrate'] += config.paths['db/migrate'].expanded unless app.root.to_s.match?(root.to_s)
+      end
+
+      config.generators do |g|
+        g.test_framework :rspec
+        g.fixture_replacement :factory_bot, dir: 'spec/factories'
+      end
+    end
+  end
+end

lib/sc/billing/railtie.rb
@@ -1,0 +1,8 @@
+# frozen_string_literal: true
+
+module SC
+  module Billing
+    class Railtie < ::Rails::Railtie
+    end
+  end
+end

lib/sc/billing/version.rb
@@ -1,0 +1,7 @@
+# frozen_string_literal: true
+
+module SC
+  module Billing
+    VERSION = '0.1.0'
+  end
+end