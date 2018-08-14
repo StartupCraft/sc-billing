a08f19a82109fe45cfcc2f67e6ffdc4431e92517
Vadim Shalamov <vadshalamov@gmail.com>
2018-08-14 09:51:28 +0400

    implement cancel subscription operation

---
+++ app/operations/sc/billing/stripe/subscriptions/cancel_operation.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module SC::Billing::Stripe::Subscriptions
+  class CancelOperation < ::SC::Billing::BaseOperation
+    def call(subscription)
+      subscription_data = cancel_in_stripe(subscription)
+      actualize_subscription(subscription, subscription_data)
+    end
+
+    private
+
+    def cancel_in_stripe(subscription)
+      ::Stripe::Subscription.retrieve(subscription.stripe_id).delete
+    end
+
+    def actualize_subscription(subscription, subscription_data)
+      ::SC::Billing::Stripe::Subscriptions::UpdateOperation.new(subscription: subscription).call(subscription_data)
+    end
+  end
+end
---
+++ spec/operations/sc/billing/stripe/subscriptions/cancel_operation_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+RSpec.describe SC::Billing::Stripe::Subscriptions::CancelOperation, :stripe do
+  subject(:call) do
+    described_class.new.call(subscription)
+  end
+
+  let(:stripe_customer) { Stripe::Customer.create(source: stripe_helper.generate_card_token) }
+  let(:stripe_subscription) { Stripe::Subscription.create(customer: stripe_customer.id) }
+  let(:user) { create(:user, stripe_customer_id: stripe_customer.id) }
+  let(:subscription) { create(:subscription, user: user, stripe_id: stripe_subscription.id) }
+
+  it 'cancels subscription' do
+    expect { call }.to(change { subscription.reload.status }.to('canceled'))
+  end
+end
