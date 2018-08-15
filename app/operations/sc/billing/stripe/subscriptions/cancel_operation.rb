3c257458ba2ddaa84a77426f84905177a616e151
Vadim Shalamov <vadshalamov@gmail.com>
2018-08-16 00:07:55 +0500

    wrap cancel subscription with try block

--- app/operations/sc/billing/stripe/subscriptions/cancel_operation.rb
+++ app/operations/sc/billing/stripe/subscriptions/cancel_operation.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
+require 'dry/monads/try'
+
 module SC::Billing::Stripe::Subscriptions
   class CancelOperation < ::SC::Billing::BaseOperation
+    include Dry::Monads::Try::Mixin
+
     def call(subscription)
-      subscription_data = cancel_in_stripe(subscription)
-      actualize_subscription(subscription, subscription_data)
+      Try(Stripe::InvalidRequestError) do
+        subscription_data = cancel_in_stripe(subscription)
+        actualize_subscription(subscription, subscription_data)
+      end
     end
 
     private
