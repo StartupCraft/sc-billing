12c335aedfc17392553489b7ec41242a7bbd5c53
Vadim Shalamov <vadshalamov@gmail.com>
2018-08-15 15:06:53 +0500

    remove append_migrations

--- lib/sc/billing/engine.rb
+++ lib/sc/billing/engine.rb
@@ -5,12 +5,6 @@ module SC
     class Engine < ::Rails::Engine
       isolate_namespace SC::Billing
 
-      initializer :append_migrations do |app|
-        config.paths['db/migrate'].expanded.each do |expanded_path|
-          app.config.paths['db/migrate'] << expanded_path
-        end
-      end
-
       config.generators do |g|
         g.test_framework :rspec
         g.fixture_replacement :factory_bot, dir: 'spec/factories'
