b8a3de00772f6c8b690cd4ffeb7adb2ab407412f
Vadim Shalamov <vadshalamov@gmail.com>
2018-09-12 17:00:05 +0500

    change symbolic link to hard

--- spec/dummy/db/migrate/20180810172253_create_stripe_plans.rb
+++ spec/dummy/db/migrate/20180810172253_create_stripe_plans.rb
@@ -1,1 +1,23 @@
-/app/db/migrate/20180810172253_create_stripe_plans.rb
+# frozen_string_literal: true
+
+Sequel.migration do
+  change do
+    create_table :stripe_plans do
+      primary_key :id
+
+      String :name, null: false
+      String :stripe_id, null: false
+      Integer :amount, null: false
+      String :currency, null: false
+      foreign_key :product_id, :stripe_products, null: false, index: true
+      String :interval, null: false
+      Integer :interval_count, null: false
+      Integer :trial_period_days
+
+      unique :stripe_id
+
+      DateTime :created_at, null: false, default: Sequel::CURRENT_TIMESTAMP
+      DateTime :updated_at, null: false, default: Sequel::CURRENT_TIMESTAMP
+    end
+  end
+end
--- spec/dummy/db/migrate/20180811114725_create_stripe_subscribed_plans.rb
+++ spec/dummy/db/migrate/20180811114725_create_stripe_subscribed_plans.rb
@@ -1,1 +1,17 @@
-/app/db/migrate/20180811114725_create_stripe_subscribed_plans.rb
+# frozen_string_literal: true
+
+Sequel.migration do
+  change do
+    create_table :stripe_subscribed_plans do
+      primary_key :id
+
+      foreign_key :subscription_id, :stripe_subscriptions, null: false, index: true
+      foreign_key :plan_id, :stripe_plans, null: false, index: true
+
+      DateTime :created_at, null: false, default: Sequel::CURRENT_TIMESTAMP
+      DateTime :updated_at, null: false, default: Sequel::CURRENT_TIMESTAMP
+
+      unique %i[subscription_id plan_id]
+    end
+  end
+end
