048f1c8c2a093851b3e398fc679f2a81a22efae8
Vadim Shalamov <vadshalamov@gmail.com>
2018-10-04 18:30:50 +0300

    randomize products names

--- spec/factories/stripe_products.rb
+++ spec/factories/stripe_products.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :stripe_product, class: 'SC::Billing::Stripe::Product' do
     to_create(&:save)
 
-    name { FFaker::Lorem.word }
+    name { SecureRandom.hex }
     stripe_id { SecureRandom.hex }
   end
 end
