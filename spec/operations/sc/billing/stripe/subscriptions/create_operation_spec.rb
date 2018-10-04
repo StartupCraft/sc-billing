bbe25ba5939a5719a2a7aa33e1a846fc76df399c
Vadim Shalamov <vadshalamov@gmail.com>
2018-10-04 18:20:06 +0300

    create subscription with trial_from_plan: true

--- spec/operations/sc/billing/stripe/subscriptions/create_operation_spec.rb
+++ spec/operations/sc/billing/stripe/subscriptions/create_operation_spec.rb
@@ -11,10 +11,33 @@ RSpec.describe SC::Billing::Stripe::Subscriptions::CreateOperation, :stripe do
   let(:user) { create(:user, stripe_customer_id: stripe_customer.id) }
   let(:plan) { stripe_helper.create_plan }
 
+  def construct_subscription(subscription_id, customer_id, subscription_items)
+    Stripe::Subscription.construct_from(
+      id: subscription_id,
+      items: subscription_items,
+      customer: customer_id,
+      status: 'active',
+      current_period_start: 1_528_385_032,
+      current_period_end: 1_530_977_032,
+      trial_start: nil,
+      trial_end: nil
+    )
+  end
+
+  before do
+    subscription_item = Stripe::SubscriptionItem.construct_from(plan: plan)
+    subscription_items = Stripe::ListObject.construct_from(data: [subscription_item])
+
+    subscription = construct_subscription('sub_1', stripe_customer.id, subscription_items)
+    allow(::Stripe::Subscription).to receive(:create).and_return(subscription)
+  end
+
   it 'creates subscription', :aggregate_failures do
     expect { call }.to change(::SC::Billing::Stripe::Subscription, :count).by(1)
 
     expect(call).to be_success
+
+    expect(::Stripe::Subscription).to have_received(:create).with(hash_including(trial_from_plan: true))
   end
 
   context 'when with coupon' do
@@ -33,6 +56,12 @@ RSpec.describe SC::Billing::Stripe::Subscriptions::CreateOperation, :stripe do
     context 'with invalid coupon' do
       let(:coupon) { SecureRandom.hex }
 
+      before do
+        error = Stripe::InvalidRequestError.new('Some error', nil)
+
+        allow(::Stripe::Subscription).to receive(:create).and_raise(error)
+      end
+
       it 'does not create subscription with coupon', :aggregate_failures do
         expect(call).to be_failure
       end
@@ -42,7 +71,8 @@ RSpec.describe SC::Billing::Stripe::Subscriptions::CreateOperation, :stripe do
   context 'when Stripe::InvalidRequestError was raised' do
     before do
       error = Stripe::InvalidRequestError.new('Some error', nil)
-      StripeMock.prepare_error(error, :create_subscription)
+
+      allow(::Stripe::Subscription).to receive(:create).and_raise(error)
     end
 
     it 'returns failure' do
@@ -53,7 +83,8 @@ RSpec.describe SC::Billing::Stripe::Subscriptions::CreateOperation, :stripe do
   context 'when Stripe::CardError was raised' do
     before do
       error = Stripe::CardError.new('Some error', nil, 402)
-      StripeMock.prepare_error(error, :create_subscription)
+
+      allow(::Stripe::Subscription).to receive(:create).and_raise(error)
     end
 
     it 'returns failure' do
