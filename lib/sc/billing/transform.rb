f67cce5e72a6e931b34ab7bf0f018a6d6d3d7373
Vadim Shalamov <vadshalamov@gmail.com>
2018-11-13 16:15:56 +0500

    add timestamp_to_time transformation

--- lib/sc/billing/transform.rb
+++ lib/sc/billing/transform.rb
@@ -17,4 +17,23 @@ module SC::Billing::Transform
       hash[key] = source_obj.respond_to?(key) ? source_obj.send(key) : nil
     end
   end
+
+  # Transform hash values from timestamp to time by keys
+  #
+  # @example
+  #   Transform[:timestamp_to_time, %i[as_of due_to]].call(as_of: 1_528_549_095, due_to: 1_528_559_09)
+  #
+  #   # => {:as_of=>2018-06-09 12:58:15 +0000, :due_to=>2018-06-09 15:44:55 +0000}
+  #
+  def self.timestamp_to_time(source_hash, keys)
+    from_timestamp_to_time_meth = ::SC::Billing::Helpers::FromTimestampToTime.new
+
+    Hash[source_hash].tap do |hash|
+      keys.each do |key|
+        next unless hash.key?(key)
+
+        hash[key] = from_timestamp_to_time_meth.call(hash[key])
+      end
+    end
+  end
 end
